27fc5f6c60e35e5c93dbf51eae7cffc199592232
scopion js <josephsiame233@gmail.com>
2022-06-05 17:28:33 +0200

    a push

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,56 +1,109 @@
 import { AddPhotoAlternate } from '@mui/icons-material'
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 let SignUp =()=>{
-    let day_dob =(e)=>{
-        console.log(e,e.target.value)
+    //TARGET WRAPPERS
+    let step1=useRef()
+    let step2=useRef()
+    let step3=useRef()
+    // SETTING STATES FOR STEP 1
+    let[userName,setUserName]=useState(null)
+    let[email,setEmail]=useState(null)
+    let[newPassword,setNewPassword]=useState(null)
+    let[re_entered_password,set_re_entered_password]=useState(null)
+    let[gender,set_gender]=useState(null)
+           //SUBMIT STEP1
+    let submit_step1 =()=>{
+        step1.current.classList.add('hide')
+        step2.current.classList.remove('hide')
     }
-    return(
+         // SETTING STATES FOR STEP 2
+    let [ looking_for,set_looking_for ]=useState(null)
+    let [interested_in,set_interested_in]=useState(null)
+    let [hobbies,set_hobbies]=useState([])
+    let [age,set_age]=useState(null)
+    let [age_error,set_age_error]=useState()
+               //adding entered hobbies
+               let entered_hobbies=[]
+    let addHobby =(e)=>{
+        if(e.target.value.length >3){
+            if(e.code == "Space" || e.key=="Enter" || e.code=="NumpadEnter" || e.code=="Comma" || e.key ==','){
+                entered_hobbies.push(e.target.value)
+                set_hobbies(entered_hobbies.concat(...hobbies))
+            e.target.value=''
+                console.log(hobbies,entered_hobbies)
+            }
+        }
+    }
+                             //SUBMIT STEP2
+      let submit_step2 =()=>{
+        step2.current.classList.add('hide')
+        step3.current.classList.remove('hide')
+    }
+                        // SETTING STATES FOR STEP 3
+    let [status,set_status]=useState(null)
+    let [min_age,set_min_age]=useState(null)
+    let [max_age,set_max_age]=useState(null)
+    let [bio,set_bio ]=useState(null)
+    let [first_date_expectations , set_first_date_expectations]=useState(null)
+    let [profile_image ,set_profile_image]=useState(null)
+    let image=useRef()
+    let add_pp=(e)=>{
+        let blob =new Blob([e.target.files[0]])
+        let _url=URL.createObjectURL(blob)
+        set_profile_image(e.target.files[0])
+        image.current.classList.remove('hide')
+        image.current.src=_url
+    }
+    return( 
         <div className='register-wrapper' >
-<div className='step1 hide' >
+            {/*step 1*/}
+<div className='step1' ref={step1} >
     <h3>register</h3>
     <div className='register-name' >
         <label>name</label>
-        <input placeholder='name' maxLength={20}  />
+        <input placeholder='name' onChange={(e)=>{setUserName(e.target.value); }}  maxLength={20}  />
     </div>
 
     <div className='register-email' >
         <label>email</label>
-        <input placeholder='email' type='email' />
+        <input onChange={(e)=>{setEmail(e.target.value) }} placeholder='email' type='email' />
     </div>
 
     <div className='register-password' >
         <label>new password</label>
-        <input placeholder='password' type='password' />
+        <input onChange={(e)=>{ setNewPassword(e.target.value) }} placeholder='password' type='password' />
     </div>
 
     <div className='register-password-repeat' >
         <label>re-enter password</label>
-        <input placeholder='re-enter password' type='password'  />
+        <input onChange={(e)=>{ set_re_entered_password(e.target.value) }} placeholder='re-enter password' type='password'  />
     </div>
 
     <div className='register-gender' >
         <label>gender</label>
-        <select onClick={day_dob} >
+        <select onChange={(e)=>{set_gender(e.target.value) }} >
             <option>male</option>
             <option>female</option>
             <option>both</option>
         </select>
     </div>
 
-    <button  >next</button>
+    <button onClick={submit_step1}  >next</button>
 </div>
-<div className='step2  hide' >
+                {/*step 2*/}
+<div ref={step2} className='step2  hide' >
     <h3>step 2</h3>
 <div className='register-looking-for' >
     <label>what are you looking for</label>
-    <select>
-        <option>a serious relationship</option>
-        <option>just friends</option>
-        <option>friends with benefits</option>
-        <option>marriage</option>
-        <option>hook ups</option>
-        <option>a fling</option>
+    <select onChange={(e)=>{ set_looking_for(e.target.value) }} >
+        <option value='serious relationship' >a serious relationship</option>
+        <option value='just friends'>just friends</option>
+        <option value='friends with benefits'>friends with benefits</option>
+        <option value='marriage'>marriage</option>
+        <option value='hook-ups'>hook ups</option>
+        <option value='fling'>a fling</option>
+        <option value='new people'>meet new people</option>
     </select>
 </div>
 
@@ -58,7 +111,7 @@ let SignUp =()=>{
     <label>
         who are you interested in
     </label>
-    <select>
+    <select onChange={(e)=>{set_interested_in(e.target.value )}} >
         <option value='men' >men</option>
         <option value='women' >women</option>
         <option value='both' >both</option>
@@ -66,8 +119,16 @@ let SignUp =()=>{
 </div>
 <div className='register-hobbies' >
     <label>hobbies</label>
-    <input />
+    <input onKeyUp={addHobby} />
 </div>
+<div style={{display:'flex' , gap:'2px',height:'auto' , flexWrap:'wrap',width:'80%',maxWidth:'80%'}} >
+        {
+            hobbies.map((hobby)=>(
+            <p style={{backgroundColor:'#EEEEEE',borderRadius:"8px",padding:'2px',fontFamily
+        :'sans-serif',fontSize:'12px',fontWeight:'700'}}>{hobby}</p>
+        ))
+        }
+    </div>
 <div className='dob' >
     <label>date of birth</label>
     <section>
@@ -118,18 +179,30 @@ let SignUp =()=>{
         <option value='11' >11</option>
         <option value='12' >12</option>
     </select>
-<input type='number' maxLength={20} />
+<input type='number'  onChange={(e)=>{
+    if(e.target.value.length ==4){
+        let current_year=new Date().getFullYear()
+    let dif=current_year-(+e.target.value)
+    set_age(dif)
+    set_age_error('')
+    }else{
+        set_age_error('must be four numbers')
+    }
+}} maxLength={4} />
+
     </section>
+    <p style={{color:'red',fontSize:"10px",fontFamily:'sans-serif',marginTop:'4px',fontWeight:'700'}} >{age_error}</p>
 </div>
-<button>next</button>
+<button onClick={submit_step2}  >next</button>
 </div>
 <div>
     </div>
-    <div className='step3' >
+                          {/*step 3*/}
+    <div ref={step3} className='step3 hide' >
         <h3>almost done !</h3>
         <div className='register-status' >
             <label>status</label>
-            <select>
+            <select onChange={(e)=>{ set_status(e.target.value) }} >
                 <option value='single' >single</option>
                 <option value='married' >married</option>
                 <option value='in a relationship' >in a relationship</option>
@@ -139,7 +212,7 @@ let SignUp =()=>{
             <label>prefered age ?</label>
 <section>
 <div><p>from</p></div>
-            <select  >
+            <select onChange={(e)=>{ set_min_age(+e.target.value) }}  >
                 <option selected value='18' >18</option>
                 <option value='19' >19</option>
                 <option value='20' >20</option>
@@ -196,7 +269,7 @@ let SignUp =()=>{
                 <option value='71' >71</option>
             </select>
             <div><p>to</p></div>
-            <select  >
+            <select onChange={(e)=>{ set_max_age(+e.target.value) }}  >
                 <option value='18' >18</option>
                 <option value='19' >19</option>
                 <option value='20' >20</option>
@@ -256,13 +329,13 @@ let SignUp =()=>{
         </div>
         <div className='register-bio' >
             <label>bio</label>
-            <textarea placeholder='optional' >
+            <textarea maxLength={30} onChange={(e)=>{ set_bio(e.target.value) }} placeholder='optional' >
 
             </textarea>
         </div>
         <div className='register-expectations' >
             <label>first date expectations?</label>
-            <input placeholder='expectations' />
+            <input onChange={(e)=>{set_first_date_expectations(e.target.value)}} maxLength={25} placeholder='expectations' />
         </div>
         <div className='register-profile'>
             <p>upload profile</p>
@@ -271,7 +344,8 @@ let SignUp =()=>{
                 <AddPhotoAlternate />
             </label>
             </div>
-            <input id='register-pic' type='file' accept='image/*'  />
+            <img src='' className='chosen-dp hide' ref={image} />
+            <input onChange={add_pp} id='register-pic' type='file' accept='image/*'  />
         </div>
         <button>done</button>
     </div>
